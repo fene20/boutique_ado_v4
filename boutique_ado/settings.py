d82e3c6f7c6894745c7596900a6344decfc6cd02
fene20 <feeneyk@gmail.com>
2021-10-24 14:40:54 +0000

    update superuser password

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -121,15 +121,21 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 # DATABASES = {
-#     'default': dj_database_url.parse('postgres://blah blah etc')
+#     'default': dj_database_url.parse('postgres://get db_url from heroku')
 # }
 
 
